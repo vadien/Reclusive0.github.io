4ba6d7c280b1ab0cf2678b2c25a815dafc084209
Reclusive0 <david.c.neill@gmail.com>
2024-06-21 15:12:35 +1000

    Add page content, info, icons, fonts

---
+++ README.md
@@ -0,0 +1,9 @@
+# David Neill - Portfolio Project
+
+Hi! This is the repo for my portfolio site, thanks for being interested enough to take a look at the backend. This version of the project has been done as part of my time with [\_nology](https://au.nology.io/), a tech training and talent provider.
+
+#### The requirements for this project:
+
+- Using [Sass](https://sass-lang.com/)/SCSS for styling.
+- Using [BEM](https://getbem.com/) methodology.
+- Placing responsiveness as a high priority. Try checking the website on your phone or tablet to see how it holds up!
--- scripts/script.js
+++ scripts/script.js
@@ -1,14 +1,15 @@
+const body = document.body;
+const menu = document.getElementById("menu");
+const me = document.getElementById("me");
+const work = document.getElementById("work");
+const links = document.querySelectorAll(".ext-link");
+
 function darkToggle() {
-  let body = document.body;
-  let menu = document.getElementById("menu");
-  let me = document.getElementById("me");
-  let work = document.getElementById("work");
   body.classList.toggle("body-dark-mode");
   menu.classList.toggle("dark");
-  me.classList.toggle("page-alt");
-  me.classList.toggle("page-alt-dark");
-  work.classList.toggle("page-alt");
-  work.classList.toggle("page-alt-dark");
+  links.forEach((e) => {
+    e.classList.toggle("ext-link--dark");
+  });
 }
 
 let scrollPosition = 0;
@@ -16,16 +17,12 @@ var navBar = document.getElementById("navbar");
 console.log(navBar);
 navBar.style.transition = "opacity 1s";
 
-setInterval(
-  document.addEventListener("scroll", (event) => navbarAppear(), 1000)
-);
-
 function navbarAppear() {
   if (window.scrollY > 600) {
     navBar.style.opacity = 1;
-    console.log("Navbar should appear");
   } else {
     navBar.style.opacity = 0;
-    console.log("Navbar should disappear");
   }
 }
+
+document.addEventListener("scroll", () => navbarAppear());
